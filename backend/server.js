941884abfd220bba06d142ce2f8e6a62261ab467
Anjalik1234 <anjalikale9876@gmail.com>
2025-07-17 23:05:13 +0530

    Updated .env usage and project files

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const dotenv = require("dotenv");
 const userRoutes = require("./routes/userRoutes");
+
+dotenv.config(); // Load environment variables
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/users", userRoutes);
 
-mongoose.connect("mongodb+srv://anjali:anjali1194@leaderboard.fgqkb1c.mongodb.net/leaderboard")
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 app.listen(5000, () => console.log("Server running on port 5000"));
